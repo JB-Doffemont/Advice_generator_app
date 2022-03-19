75c7c42435443e2d225b7b25c7db7a8a7c06ee9e
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-19 12:47:21 +0100

    commit event click btn

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,11 @@
-const hello = "bonjour";
+document.addEventListener("DOMContentLoaded", () => {
+  // Call element from DOM
+  const advice = document.getElementsByClassName("main__adviceSection");
+  const btn = document.getElementById("adviceBtn");
 
-console.log(hello);
+  console.log(advice);
+
+  btn.addEventListener("click", () => {
+    console.log("Ca marche");
+  });
+});
