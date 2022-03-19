b0143e37feecc11d3a274bdce548dc558b1eb01c
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-19 14:43:48 +0100

    commit display advice + id

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Call element from DOM
   const advice = document.getElementById("adviceText");
+  const id = document.getElementById("adviceId");
   const btn = document.getElementById("adviceBtn");
 
   console.log(advice);
@@ -10,20 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
     async function getAdvice() {
       try {
         const response = await fetch("https://api.adviceslip.com/advice");
-
         if (!response.ok) {
           throw new Error(`Error! status:${response.status}`);
         }
-
         const result = await response.json();
         const data = result.slip;
         console.log(data);
+        id.innerHTML = data.id;
+        advice.innerText = '"' + data.advice + '"';
         return result;
       } catch (err) {
         console.log(err);
       }
     }
-
     getAdvice();
   });
 });
