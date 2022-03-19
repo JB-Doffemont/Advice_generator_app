1f8ca5a6e288b22e55521a7b768724cfa86debf9
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-19 14:34:15 +0100

    commit request API

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,8 +5,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   console.log(advice);
 
+  // Event onclick request API to get advice
   btn.addEventListener("click", () => {
-    advice.innerText = "yo";
-    console.log("Ca marche");
+    async function getAdvice() {
+      try {
+        const response = await fetch("https://api.adviceslip.com/advice");
+
+        if (!response.ok) {
+          throw new Error(`Error! status:${response.status}`);
+        }
+
+        const result = await response.json();
+        const data = result.slip;
+        console.log(data);
+        return result;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getAdvice();
   });
 });
