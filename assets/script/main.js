1c603e327c9c4cee81cb2bfbb34197e4cbe2092e
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-19 14:53:57 +0100

    commit function request when dom loaded

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,19 +4,46 @@ document.addEventListener("DOMContentLoaded", () => {
   const id = document.getElementById("adviceId");
   const btn = document.getElementById("adviceBtn");
 
-  console.log(advice);
+  // function to interact with API
+  async function getAdvice() {
+    try {
+      // request to API
+      const response = await fetch("https://api.adviceslip.com/advice");
+      if (!response.ok) {
+        throw new Error(`Error! status:${response.status}`);
+      }
+      // await for response in json
+      const result = await response.json();
+      // getting data
+      const data = result.slip;
+      console.log(data);
 
-  // Event onclick request API to get advice
+      // display content
+      id.innerHTML = data.id;
+      advice.innerText = '"' + data.advice + '"';
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  getAdvice();
+
+  // Event onclick to fire new request API
   btn.addEventListener("click", () => {
-    async function getAdvice() {
+    async function getAdviceOnClick() {
       try {
+        // request to API
         const response = await fetch("https://api.adviceslip.com/advice");
         if (!response.ok) {
           throw new Error(`Error! status:${response.status}`);
         }
+        // await for response in json
         const result = await response.json();
+        // getting data
         const data = result.slip;
         console.log(data);
+
+        // display content
         id.innerHTML = data.id;
         advice.innerText = '"' + data.advice + '"';
         return result;
@@ -24,6 +51,6 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(err);
       }
     }
-    getAdvice();
+    getAdviceOnClick();
   });
 });
