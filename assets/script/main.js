7d142c724f1dfe3e19fadafeecaf065565f7f23a
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-19 12:54:40 +0100

    commit advice innerText on click

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Call element from DOM
-  const advice = document.getElementsByClassName("main__adviceSection");
+  const advice = document.getElementById("adviceText");
   const btn = document.getElementById("adviceBtn");
 
   console.log(advice);
 
   btn.addEventListener("click", () => {
+    advice.innerText = "yo";
     console.log("Ca marche");
   });
 });
