9f46b82c12781bdc64b90261d66a3f61f901e779
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-20 14:22:43 +0100

    commit trim down js

--- assets/script/main.js
+++ assets/script/main.js
@@ -30,27 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Event onclick to fire new request API
   btn.addEventListener("click", () => {
-    async function getAdviceOnClick() {
-      try {
-        // request to API
-        const response = await fetch("https://api.adviceslip.com/advice");
-        if (!response.ok) {
-          throw new Error(`Error! status:${response.status}`);
-        }
-        // await for response in json
-        const result = await response.json();
-        // getting data
-        const data = result.slip;
-        console.log(data);
-
-        // display content
-        id.innerHTML = data.id;
-        advice.innerText = '"' + data.advice + '"';
-        return result;
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    getAdviceOnClick();
+    getAdvice();
   });
 });
